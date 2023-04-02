54c26c9ba109c946c21d1040b954c216df8186a9
AtharvaDeshmukh0909 <atharvadeshmukh0909@gmail.com>
2023-04-02 16:20:08 +0530

    ui added

---
+++ Assets/AI_MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AI_MainMenu : MonoBehaviour
+{
+    public void Easy()
+    {
+        // SceneManager.LoadScene(1);
+    }
+    public void Medium()
+    {
+        // SceneManager.LoadScene(1);
+    }
+    public void Hard()
+    {
+        // SceneManager.LoadScene(1);
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/LevelMainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMainMenu : MonoBehaviour
+{
+    public void Level1()
+    {
+        // SceneManager.LoadScene(1);
+    }
+    public void Level2()
+    {
+        // SceneManager.LoadScene(1);
+    }
+    public void Level3()
+    {
+        // SceneManager.LoadScene(1);
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
